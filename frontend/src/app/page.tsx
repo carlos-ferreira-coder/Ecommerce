970b86439a5ecb28943d55c19b17b360d0419d12
Carlos S. Ferreira <ferreira.carlos.bcs@gmail.com>
2026-06-19 11:33:56 -0300

    feat: grid

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <div></div>;
+  return (
+    <div className="w-full max-w-360 mx-auto px-5 lg:px-27">Hello World</div>
+  );
 }
---
+++ frontend/src/components/ui/grid.tsx
@@ -0,0 +1,19 @@
+import { ReactNode } from "react";
+
+interface GridProps {
+  children: ReactNode;
+}
+
+export function Grid({ children }: GridProps) {
+  return (
+    <div
+      className="
+        grid
+        grid-cols-4 lg:grid-cols-12
+        gap-4 lg:gap-6
+      "
+    >
+      {children}
+    </div>
+  );
+}
