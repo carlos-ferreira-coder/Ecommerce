940f11d3841c75c58923fdc1e15f6cbea0b73b4c
Carlos S. Ferreira <ferreira.carlos.bcs@gmail.com>
2026-06-21 13:24:03 -0300

    refactor: use clsx

--- frontend/src/components/ui/gridOverlay.tsx
+++ frontend/src/components/ui/gridOverlay.tsx
@@ -1,3 +1,5 @@
+import clsx from "clsx";
+
 const GRID_OVERLAY = process.env.NEXT_PUBLIC_GRID_OVERLAY === "true";
 
 interface GridProps {
@@ -16,10 +18,10 @@ export default function GridOverlay({ children }: GridProps) {
           {Array.from({ length: 12 }).map((_, index) => (
             <div
               key={index}
-              className={`
-                  border border-red-500/40 bg-red-500/10
-                  ${index >= 4 ? "hidden lg:block" : ""}
-                `}
+              className={clsx(
+                "border border-red-500/40 bg-red-500/10",
+                index >= 4 && "hidden lg:block",
+              )}
             />
           ))}
         </div>
