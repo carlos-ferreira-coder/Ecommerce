e5c48754d73db46257e837e0c1dcbc32b07d0aa9
Carlos S. Ferreira <ferreira.carlos.bcs@gmail.com>
2026-06-21 02:06:13 -0300

    fix: semantic error

--- frontend/src/components/header/overlay/menu/authButtons.tsx
+++ frontend/src/components/header/overlay/menu/authButtons.tsx
@@ -4,13 +4,13 @@ import ProfileIcon from "@/components/icons/profileIcon";
 export default function ButtonAuth() {
   return (
     <div className="flex justify-between gap-4 py-4 border-t border-gray-400">
-      <link
+      <Link
         href="/login"
         className="flex items-center justify-center gap-1 h-10 w-full text-button-sm text-primary-600 border border-primary-600"
       >
         <ProfileIcon fill="var(--color-gray-900)" />
         Login
-      </link>
+      </Link>
 
       <Link
         href="/register"
