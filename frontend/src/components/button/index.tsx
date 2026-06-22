70a142643ccb292ea019d1f1e5b410160149f018
Carlos S. Ferreira <ferreira.carlos.bcs@gmail.com>
2026-06-22 14:15:11 -0300

    feat: create dark background

--- frontend/src/components/button/index.tsx
+++ frontend/src/components/button/index.tsx
@@ -2,12 +2,35 @@ import clsx from "clsx";
 import { twMerge } from "tailwind-merge";
 import { ComponentProps } from "react";
 
+const styles = {
+  default:
+    "flex items-center justify-center h-10 lg:h-12 px-6 gap-1 text-button-sm lg:text-button-lg shadow-md",
+  interactive:
+    "cursor-pointer transition-all duration-200 active:shadow-sm active:translate-y-0.5 lg:hover:-translate-y-0.5 lg:active:translate-y-0",
+  light: {
+    fill: "bg-primary-600 hover:bg-primary-700 active:bg-primary-750 text-white",
+    stroke:
+      "border border-primary-600 hover:border-primary-700 active:border-primary-750 text-primary-600 hover:text-primary-700 active:text-primary-750",
+    text: "text-primary-600 hover:text-primary-700 active:text-primary-750",
+    disabled: "bg-gray-400 text-gray-500 cursor-not-allowed",
+  },
+  dark: {
+    fill: "bg-white hover:bg-gray-200 active:bg-gray-300 text-black",
+    stroke:
+      "border border-white hover:border-gray-200 active:border-gray-300 text-white hover:text-gray-200 active:text-gray-300",
+    text: "text-white hover:text-gray-200 active:text-gray-300",
+    disabled: "bg-gray-700 text-gray-500 cursor-not-allowed",
+  },
+} as const;
+
 interface ButtonProps extends ComponentProps<"button"> {
   variant?: "fill" | "stroke" | "text";
+  background?: "light" | "dark";
 }
 
 export default function Button({
   variant = "fill",
+  background = "light",
   disabled,
   className,
   children,
@@ -15,24 +38,12 @@ export default function Button({
 }: ButtonProps) {
   return (
     <button
+      disabled={disabled}
       className={twMerge(
         clsx(
-          "flex items-center justify-center h-10 lg:h-12 px-6 gap-1 text-button-sm lg:text-button-lg shadow-md",
-          !disabled &&
-            "cursor-pointer transition-all duration-200 active:shadow-sm active:translate-y-0.5 lg:hover:-translate-y-0.5 lg:active:translate-y-0",
-          {
-            // Fill
-            "bg-primary-600 active:bg-primary-750 text-white hover:bg-primary-700":
-              variant === "fill" && !disabled,
-            // Stroke
-            "border border-primary-600 hover:border-primary-700 active:border-primary-750 text-primary-600 hover:text-primary-700 active:text-primary-750":
-              variant === "stroke" && !disabled,
-            // Text
-            "text-primary-600 hover:text-primary-700 active:text-primary-750":
-              variant === "text" && !disabled,
-            // Disabled
-            "bg-gray-400 text-gray-500 cursor-not-allowed": disabled,
-          },
+          styles["default"],
+          !disabled && styles["interactive"],
+          disabled ? styles[background].disabled : styles[background][variant],
           className,
         ),
       )}
--- frontend/src/components/header/overlay/shoppingBag/index.tsx
+++ frontend/src/components/header/overlay/shoppingBag/index.tsx
@@ -4,11 +4,12 @@ import ShoppingBagIcon from "@/components/icons/shoppingBagIcon";
 import ButtonOverlay from "@/components/header/overlay/button";
 import OverlayHeader from "@/components/header/overlay/index";
 import Button from "@/components/button";
+import MenuIcon from "@/components/icons/menuIcon copy";
 
 export default function ShoppingBagWrapperOverlayHeader() {
   return (
     <ButtonOverlay
-      icon={<ShoppingBagIcon quantity={10} />}
+      icon={<ShoppingBagIcon quantity={5} />}
       overlayType="shopping-bag"
       OverlayComponent={ShoppingBagOverlayHeader}
       showCloseIcon={false}
@@ -20,20 +21,32 @@ export function ShoppingBagOverlayHeader() {
   return (
     <OverlayHeader>
       <div className="flex items-center justify-center h-20 gap-2">
-        <Button variant="fill">test</Button>
-        <Button variant="stroke">test</Button>
-        <Button variant="text">test</Button>
+        <Button variant="fill">
+          <MenuIcon /> test
+        </Button>
+        <Button variant="stroke">
+          <MenuIcon /> test
+        </Button>
+        <Button variant="text">
+          <MenuIcon /> test
+        </Button>
+        <Button disabled>
+          <MenuIcon /> test
+        </Button>
       </div>
 
-      <div className="flex items-center justify-center h-20 gap-2">
-        <Button variant="fill" disabled>
-          test
+      <div className="flex items-center justify-center h-20 gap-2 bg-gray-800">
+        <Button variant="fill" background="dark">
+          <MenuIcon /> test
         </Button>
-        <Button variant="stroke" disabled>
-          test
+        <Button variant="stroke" background="dark">
+          <MenuIcon /> test
         </Button>
-        <Button variant="text" disabled>
-          test
+        <Button variant="text" background="dark">
+          <MenuIcon /> test
+        </Button>
+        <Button background="dark" disabled>
+          <MenuIcon /> test
         </Button>
       </div>
     </OverlayHeader>
