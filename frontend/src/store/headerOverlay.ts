93cb6e61f0f4a2d017452d4a9955a3aa2c6eca29
Carlos S. Ferreira <ferreira.carlos.bcs@gmail.com>
2026-06-23 12:57:00 -0300

    fix: name

--- frontend/src/store/headerOverlay.ts
+++ frontend/src/store/headerOverlay.ts
@@ -2,20 +2,22 @@ import { create } from "zustand";
 
 export type HeaderOverlayType = "menu" | "search" | "shopping-bag" | null;
 
-interface HeaderOverlayStoreProps {
+interface useHeaderOverlayStoreProps {
   headerOverlay: HeaderOverlayType;
   toggleHeaderOverlay: (overlay: HeaderOverlayType) => void;
   closeHeaderOverlay: () => void;
 }
 
-export const useHeaderOverlayStore = create<HeaderOverlayStoreProps>((set) => ({
-  headerOverlay: null,
-  toggleHeaderOverlay: (overlay) =>
-    set((state) => ({
-      headerOverlay: state.headerOverlay === overlay ? null : overlay,
-    })),
-  closeHeaderOverlay: () =>
-    set({
-      headerOverlay: null,
-    }),
-}));
+export const useHeaderOverlayStore = create<useHeaderOverlayStoreProps>(
+  (set) => ({
+    headerOverlay: null,
+    toggleHeaderOverlay: (overlay) =>
+      set((state) => ({
+        headerOverlay: state.headerOverlay === overlay ? null : overlay,
+      })),
+    closeHeaderOverlay: () =>
+      set({
+        headerOverlay: null,
+      }),
+  }),
+);
